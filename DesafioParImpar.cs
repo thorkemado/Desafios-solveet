f4ffccf3334ade260b2671145715fa1605564496
thorkemado <jvdochaofernandez@gmail.com>
2016-03-09 17:03:45 +0100

    Solución suma par impares
    Esta es una solución Recursiva al problema.

---
+++ DesafioParImpar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafios
+{
+    class DesafioParImpar
+    {
+
+       public void sumaParImpar(int num, out int SumaPar, out int SumaImpar)
+        {
+            int aux = num - 1;
+            if (num == 1)
+            {
+                SumaImpar = 0;
+                SumaPar = 0;
+            }
+            else
+            {
+                if (aux % 2 == 0)
+                {
+                    sumaParImpar(aux, out SumaPar, out SumaImpar);
+                    SumaPar += aux;
+                }
+                else
+                {
+                    sumaParImpar(aux, out SumaPar, out SumaImpar);
+                    SumaImpar += aux;
+                }
+            }
+        }
+    }
+}
